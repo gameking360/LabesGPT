b561f5cae76c70fb1d4527c928453b18d609faf7
Labes <gdlabes8@gmail.com>
2024-06-20 16:00:18 -0300

    Alteração no input do Request e correçao do gitignore

--- GroqGPT/Program.cs
+++ GroqGPT/Program.cs
@@ -8,88 +8,29 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        using (var cts = new CancellationTokenSource())
+
+        GroqClient client = new GroqClient("llama3-70b-8192")
+                     .SetTemperature(0.5)
+                     .SetMaxTokens(256)
+                     .SetTopP(1)
+                     .SetStop("NONE");
+
+
+        while (true)
         {
+            Console.Write("Sua mensagem: ");
 
-            Thread checar = new Thread(() => CheckEndProgram(cts));
+            string mensagem = Console.ReadLine();
 
-            checar.Start();
+            var response = await client.SendMessage(new MessageModel { Role = MessageRole.user, Content = mensagem });
 
-                            
-
-                GroqClient client = new GroqClient("llama3-70b-8192")
-                    .SetTemperature(0.5)
-                    .SetMaxTokens(256)
-                    .SetTopP(1)
-                    .SetStop("NONE");
-
-
-                while (!cts.Token.IsCancellationRequested)
-                {
-                    Console.WriteLine("Sua mensagem");
-
-                    string mensagem = await ConsoleReadAsync(cts);
-
-                    var response = await client.SendMessage(new MessageModel { Role = MessageRole.user, Content = mensagem });
-
-                    foreach (var msg in response.Contents)
-                    {
-                        Console.WriteLine("Resposta do bot:");
-                        Console.WriteLine(msg);
-                    }
-                    await Task.Delay(100);
-                }
-            }
-       }
-
-    private static void CheckEndProgram(CancellationTokenSource cts)
-    {
-        while (!cts.Token.IsCancellationRequested)
-        {
-            if (Console.KeyAvailable)
+            foreach (var msg in response.Contents)
             {
-                var key = Console.ReadKey(intercept : true);
-                if(key.Key == ConsoleKey.Escape)
-                {
-                   Console.WriteLine("Programa cancelado.");
-                   Environment.Exit(10);
-                }
+                Console.WriteLine("\nResposta do bot:");
+                Console.WriteLine(msg);
             }
-        }
-    }
-
-    private static async Task<string> ConsoleReadAsync(CancellationTokenSource cts)
-    {
-        var input = new System.Text.StringBuilder();
-
-        while (!cts.Token.IsCancellationRequested)
-        {
-
-            if (Console.KeyAvailable)
-            {
-
-
-                var key = Console.ReadKey(intercept: true);
-
-
-                if (key.Key == ConsoleKey.Enter)
-                {
-                    Console.WriteLine();
-                    return input.ToString();
-                }
-                else if (key.Key == ConsoleKey.Backspace && input.Length > 0)
-                {
-                    input.Remove(input.Length - 1, 1);
-                    Console.Write("\b \b");
-                }
-                else
-                {
-                    input.Append(key.KeyChar);
-                    Console.Write(key.KeyChar);
-                }
-            }
+            await Task.Delay(100);
         }
 
-        return null;
     }
 }
